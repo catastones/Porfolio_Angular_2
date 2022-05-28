a777cd3cda630374db3f5005982479d827133b09
Cristian Alderete <catastones@gmail.com>
2022-05-28 17:28:21 -0300

    enviar login recibir token

--- src/app/Componentes/Porfolio/navbar/login/login.component.ts
+++ src/app/Componentes/Porfolio/navbar/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PorfolioDataService } from 'src/app/servicios/porfolio-data.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  user: any;
+  constructor(private datosporfolio: PorfolioDataService) { }
+  formLogin = new FormGroup({
+    user: new FormControl('', Validators.required),
+    pass: new FormControl('', Validators.required)
+  });
   ngOnInit(): void {
   }
+  sumit() {
+    // console.log(this.formLogin.value);
+    // console.log(this.formLogin.value.pass);
 
+
+    this.datosporfolio.login(this.formLogin.value.user, this.formLogin.value.pass).subscribe(data => {
+      this.user = data;
+      console.log(this.user.body);
+    });
+  }
 }
--- src/app/servicios/porfolio-data.service.ts
+++ src/app/servicios/porfolio-data.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, ObservableInput, Subject, tap } from 'rxjs';
+
+import { Observable, Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,10 @@ import { Observable, ObservableInput, Subject, tap } from 'rxjs';
 export class PorfolioDataService {
 
   private _refresh$ = new Subject<void>();// se genara observable para refrescar los componentes ante un cambio de datos
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+
+  }
   get refresh$() {
     return this._refresh$;
   }
@@ -46,18 +50,22 @@ export class PorfolioDataService {
     return this.http.get('./assets/data/estado.json');
   }
 
+  options = {
+    headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+  };
+
+
   login(user: string, password: string): Observable<any> {
     const body = new HttpParams()
-      .set(`user`, user)
-      .set(`password`, password);
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
-    return this.http.post('http://localhost:8080/api/login', body.toString(), { headers, observe: 'response' })
-      .pipe(
-        tap(
-          () => { this._refresh$.next(); })
-      )
-    // return this.http.post(`auth/login`, body.toString(), { headers, observe: 'response' })
-    //   .map((res: HttpResponse<Object>) => res.ok)
-    //   .catch((err: any) => Observable.of(false));
+      .set(`usuario`, user)
+      .set(`Pass`, password);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    return this.http.post('http://localhost:8080/api/login', body,
+      { headers, observe: 'response' })
   }
+
+
 }
